e9a76716629ac56a217385d1e783ff197d2576f4
jaeilssss <jaeilssss@gmail.com>
2024-02-17 17:58:50 +0900

    init project

---
+++ private-note/src/main/java/com/example/privatenote/PrivateNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.privatenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateNoteApplication.class, args);
+    }
+
+}
---
+++ private-note/src/test/java/com/example/privatenote/PrivateNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.privatenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
